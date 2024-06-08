d943e7ed382adc74bb2e7aa3f0e065ab3a8bf826
Shivang Rathore <rshivang12345@gmail.com>
2024-06-08 13:32:21 +0530

    test: implementation

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,20 @@
+from typing import List
+import discord
 from discord.ext.better_menus import AsyncIteratorPageSource
+
+
+async def test_generator():
+    for x in range(50):
+        yield x
+
+
+
+class MySource(AsyncIteratorPageSource):
+    async def format_page(self, page: List) -> discord.Embed:
+        print(page)
+        return discord.Embed()
+    
+    async def get_num_entries() -> int:
+        return 10
+    
+MySource(test_generator(), 10)
